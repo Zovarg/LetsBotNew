7e67c95f96a4ad57bd372eeb5bf961e55d25dc2b
Zovarg <Zovavrg@mail.ru>
2022-09-30 00:42:06 +0300

    Project_add

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import VueI18n from 'vue-i18n'
+import vueI18nEsm from "vue-i18n";
+Vue.use(VueI18n)
 
 Vue.config.productionTip = false
 
+const i18n = new VueI18n({
+
+})
+
 new Vue({
-  router,
-  store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
